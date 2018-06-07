dd2520af56546bcc44466d5a46177d98c5f5fd60
Jedai Saboteur <dev@paradoxinversion.com>
2018-06-07 15:03:26 -0700

    Remove express-session

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const express = require("express");
-const session = require("express-session");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -16,31 +15,13 @@ app.use(logger("dev"));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: true,
-    saveUninitialized: true
-  })
-);
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, "public")));
 
 passport.use(
   new LocalStrategy(async function(username, password, done) {
-    // User.findOne({ username: username }, function(err, user) {
-    //   if (err) {
-    //     return done(err);
-    //   }
-    //   if (!user) {
-    //     return done(null, false);
-    //   }
-    //   if (!user.verifyPassword(password)) {
-    //     return done(null, false);
-    //   }
-    //   return done(null, user);
-    // });
     try {
       const user = await getUserByName(username);
       if (!user) return done(null, false);
@@ -63,16 +44,7 @@ passport.deserializeUser(function(id, done) {
 });
 mongooseClient();
 const index = require("./routes/index");
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-//   next();
-// });
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
